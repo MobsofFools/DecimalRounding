54bb7d05c30b1e2aaf9d763a1a269e35f801f648
MobsofFools <edvinlin@msn.com>
2023-08-16 16:19:13 -0600

    initial commit

---
+++ DecimalRounding/DecimalControl.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { ChangeEvent, FocusEvent, useState } from "react";
+import { IInputs } from "./generated/ManifestTypes";
+
+export type IDecimalControlProps = {
+    onChange:(numberData: number|undefined) => void;
+    context?:ComponentFramework.Context<IInputs>;
+}
+const DecimalControl = (props:IDecimalControlProps) => {
+    const {onChange, context} = props;
+    const [controlValue, setControlValue] = useState<number|string>(context?.parameters.boundField.raw!);
+    const roundDirection = context?.parameters.roundDirection.raw!;
+    function numberRounding (number:number,direction?:string) {
+        if(direction === "up"){
+            return Math.ceil(number)
+        }
+        else{
+            return Math.floor(number)
+        }
+    }
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setControlValue(e.currentTarget.value);
+        console.log(e.currentTarget.value)
+    }
+    const handleBlur = (e: FocusEvent<HTMLInputElement>) => {
+        if(typeof controlValue === 'string')
+        {
+            const result = numberRounding(Number.parseFloat(controlValue),roundDirection)
+            setControlValue(result);
+            onChange(result)
+        }
+    }
+    return(
+        <div>
+            <input value={controlValue} onChange={handleChange} onBlur={handleBlur}></input>
+        </div>
+    )
+}
+export default DecimalControl
